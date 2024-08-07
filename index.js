aef4a404b2d9decb6686901655357bcf1e7c8e9d
Your Name <you@example.com>
2024-08-07 08:24:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let posts = [];
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", {posts: posts});
+})
+
+app.post("/submit", (req, res) => {
+    const post = {
+        title: req.body.title,
+        image: req.body.image,
+        content: req.body.content
+    }
+    posts.push(post);
+    res.redirect('/');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
